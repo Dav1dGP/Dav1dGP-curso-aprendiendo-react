c638386ae121e928c1ca22eb75ca80b5e9e650df
DavidGaspar <david.gaspar.alfaz@gmail.com>
2024-04-29 11:56:23 +0200

    Logica del tablero, ajustando el final del juego y detectar ganador.

--- projects/tic-tac-toe/src/App.jsx
+++ projects/tic-tac-toe/src/App.jsx
@@ -14,16 +14,27 @@ const Square = ({ children, isSelected, updateBoard, index}) => {
   const className = `square ${isSelected ? 'is-selected' : ''}`
 
   const handleClick = () => {
-    updateBoard()
+    updateBoard(index)
   }
 
   return (
-    <div onClick={handleClick} className="className">
+    <div onClick={handleClick} className={className}>
       {children}
     </div>
   )
 }
 
+const WINNER_COMBOS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+
+]
 
 function App() {
   {/* El board crea un array de 9 elementos y los rellena con el valor null */}
@@ -31,19 +42,54 @@ function App() {
     Array(9).fill(null)
   )
   const [turn, setTurn] = useState(TURNS.X)
+  // null es que no hay ganador, false es que hay un empate
+  const[winner, setWinner] = useState(null) 
 
-  const updateBoard = () => {
+  const checkWinner = (boardToCheck) => {
+    // revisamos todas las combinacioens ganadores para ver si X u = ganó
+    for (const combo of WINNER_COMBOS){
+      const [a,b,c] = combo
+    
+    if(
+      boardToCheck[a] &&
+      boardToCheck[a] === boardToCheck[b] &&
+      boardToCheck[a] === boardToCheck[c]
+    ) {
+      return boardToCheck[a]
+    }
+  }
+  //si no hay ganador
+  return null
 
   }
 
+  const updateBoard = (index) => {
+    // no actualizar esta posición si ya tiene algo
+    if (board[index] || winner) return
+    //newBoard igual a board, se hace un Spread Operator
+    const newBoard = [... board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+    //cambiar e turno
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+    // revisar si hay un ganador
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+    }
+  }
+
   return (
-    <main className='board' >
+    <main className='board'>
       <h1>Tic-Tac-Toe</h1>
       <section className="game">
       {
+        
         board.map(( square, index) => {
           return (
-            <Square 
+            
+            <Square // Componente que hemos hecho para cada uno de los cuadritos
               key={index}
               index={index}
               updateBoard={updateBoard}
