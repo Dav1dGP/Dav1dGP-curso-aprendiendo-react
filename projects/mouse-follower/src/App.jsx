5e1480610cff6a9ead401c683ae9a8421207956a
DavidGaspar <david.gaspar.alfaz@gmail.com>
2024-04-29 19:39:28 +0200

    Cambio titulo y empezado UseEffect

--- projects/mouse-follower/src/App.jsx
+++ projects/mouse-follower/src/App.jsx
@@ -1,11 +1,20 @@
-
+import { useEffect, useState } from 'react' 
 import './App.css'
 
 function App() {
-  
+  const [enabled, setEnabled] = useState(false)
+
+  useEffect(() => {
+    console.log('efecto')
+  })
 
   return (
+    <>
     <h1>Proyecto 3</h1>
+    <button onClick={() => setEnabled(!enabled)}>
+      {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+      </button>
+    </>
   )
 }
 
